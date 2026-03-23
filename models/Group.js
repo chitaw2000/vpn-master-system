875d6f6c9cad815b7a9cf436422433f6f740f282
Min Thant Kyaw <98321994+chitaw2000@users.noreply.github.com>
2026-03-23 22:28:00 +0700

    Update Group.js

--- models/Group.js
+++ models/Group.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 
 const groupSchema = new mongoose.Schema({
     name: { type: String, required: true, unique: true },
+    masterGroupId: { type: String, required: true }, // 🌟 Master Panel ဘက်က Group ID
     createdAt: { type: Date, default: Date.now }
 });
 
