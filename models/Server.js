b90f0b1cf438f77bd60f99fda06042af6f26dd69
Min Thant Kyaw <98321994+chitaw2000@users.noreply.github.com>
2026-03-23 22:13:59 +0700

    Update Server.js

--- models/Server.js
+++ models/Server.js
@@ -1,25 +1,9 @@
-require('dotenv').config();
-const express = require('express');
-require('./config/db')();
+const mongoose = require('mongoose');
 
-const adminRoutes = require('./routes/admin');
-const userRoutes = require('./routes/user');
+const serverSchema = new mongoose.Schema({
+    groupName: { type: String, required: true }, // ဥပမာ - ⭐ VIP Asia
+    serverName: { type: String, required: true }, // ဥပမာ - SG-VIP-1
+    serverKey: { type: String, required: true }   // ss://...
+});
 
-const adminApp = express();
-const userApp = express();
-
-adminApp.use(express.json());
-adminApp.use(express.urlencoded({ extended: true }));
-userApp.use(express.json());
-userApp.use(express.urlencoded({ extended: true }));
-
-adminApp.use('/admin', adminRoutes);
-adminApp.use('/api/internal', adminRoutes);
-userApp.use('/', userRoutes);
-
-const ADMIN_PORT = process.env.ADMIN_PORT || 4000;
-const USER_PORT = process.env.USER_PORT || 3000;
-const VPS_IP = process.env.VPS_IP || '127.0.0.1';
-
-adminApp.listen(ADMIN_PORT, () => console.log(`🚀 Admin Dashboard: http://${VPS_IP}:${ADMIN_PORT}/admin`));
-userApp.listen(USER_PORT, () => console.log(`🚀 User Panel     : http://${VPS_IP}:${USER_PORT}/panel`));
+module.exports = mongoose.model('Server', serverSchema);
