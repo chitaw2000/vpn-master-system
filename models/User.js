6f30e1322888ec67dcbf6434266c49e5c7f7bea9
Min Thant Kyaw <98321994+chitaw2000@users.noreply.github.com>
2026-03-23 20:46:22 +0700

    Update User.js

--- models/User.js
+++ models/User.js
@@ -3,10 +3,11 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
     token: { type: String, required: true, unique: true },
     name: { type: String, required: true },
-    groupName: { type: String, required: true }, // 🆕 User ပိုင်ဆိုင်သော Group
+    groupName: { type: String, required: true }, 
     currentServer: { type: String, required: true },
     usedGB: { type: Number, default: 0 },
-    totalGB: { type: Number, required: true }
+    totalGB: { type: Number, required: true },
+    expireDate: { type: String, required: true } // 🆕 Date အတွက် အသစ်
 });
 
 module.exports = mongoose.model('User', userSchema);
