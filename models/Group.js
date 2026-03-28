98c668610f6ba36fe7093377599adef148189113
Min Thant Kyaw <98321994+chitaw2000@users.noreply.github.com>
2026-03-28 13:33:28 +0700

    Update Group.js

--- models/Group.js
+++ models/Group.js
@@ -4,8 +4,9 @@ const groupSchema = new mongoose.Schema({
     name: { type: String, required: true, unique: true },
     masterGroupId: { type: String, required: true },
     nsRecord: { type: String, required: true },
-    masterIp: { type: String, required: true },       // 🌟 Master Panel IP အသစ်
-    masterApiKey: { type: String, required: true }    // 🌟 သက်ဆိုင်ရာ API Key
+    masterIp: { type: String, required: true },
+    masterApiKey: { type: String, required: true },
+    masterName: { type: String, default: "1" } // 🌟 API နံပါတ် မှတ်ရန် အသစ်
 });
 
 module.exports = mongoose.model('Group', groupSchema);
