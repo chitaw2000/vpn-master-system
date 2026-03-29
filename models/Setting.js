66d0508f28732179741f379dc43613b482b0c9f5
Min Thant Kyaw <98321994+chitaw2000@users.noreply.github.com>
2026-03-29 21:23:25 +0700

    Update Setting.js

--- models/Setting.js
+++ models/Setting.js
@@ -1,10 +1,9 @@
-// File: models/Setting.js
 const mongoose = require('mongoose');
 
 const settingSchema = new mongoose.Schema({
     botToken: { type: String, default: '' },
     adminId: { type: String, default: '' },
-    backupIntervalHours: { type: Number, default: 1 }
+    backupIntervalMinutes: { type: Number, default: 60 } // 🌟 ဤနေရာတွင် Minutes အဖြစ် ပြောင်းထားပါသည်
 });
 
 module.exports = mongoose.model('Setting', settingSchema);
